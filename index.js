8c3b79a446cac90ce3fb5061d7e7c03c67201d74
Anthony Scotti <anthony.m.scotti@gmail.com>
2023-08-16 20:41:45 -0400

    Default options to true

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const DEFAULT = { pre: false, post: false }
+const DEFAULT = { pre: true, post: true }
 
 /**
  * Trims the specified target string from the start or end of the input string, depending on the provided options.
  *
  * @param {string} str - The input string.
  * @param {string} target - The target string to remove.
- * @param {Object} [options={ pre: false, post: false }] - Options to determine whether to trim from the start (pre) or end (post).
+ * @param {Object} [options={ pre: true, post: true }] - Options to determine whether to trim from the start (pre) or end (post).
  * @param {boolean} [options.pre=false] - Whether to trim target from the start of the input string.
  * @param {boolean} [options.post=false] - Whether to trim target from the end of the input string.
  * @returns {string} The result string after trimming.
@@ -32,9 +32,9 @@ export function trimEdge (str, target, options = DEFAULT) {
    * Creates a function that trims the specified target string from the start or end of any provided string.
    *
    * @param {string} target - The target string to remove.
-   * @param {Object} [options={ pre: false, post: false }] - Options to determine whether to trim from the start (pre) or end (post).
-   * @param {boolean} [options.pre=false] - Whether to trim target from the start.
-   * @param {boolean} [options.post=false] - Whether to trim target from the end.
+   * @param {Object} [options={ pre: true, post: true }] - Options to determine whether to trim from the start (pre) or end (post).
+   * @param {boolean} [options.pre=true] - Whether to trim target from the start.
+   * @param {boolean} [options.post=true] - Whether to trim target from the end.
    * @returns {function(string): string} A function that takes an input string and returns the result string after trimming.
    */
 export const createTrimEdgeFn = (target, options = DEFAULT) => {
